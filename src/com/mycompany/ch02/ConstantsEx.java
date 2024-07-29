6a2ebe68b6ed3296477072d1df1b1d4879d1b7e1
imdoyoung <bear2008@naver.com>
2024-07-29 10:11:36 +0900

    ch02-constantEx(상수)

---
+++ src/com/mycompany/ch02/ConstantsEx.java
@@ -0,0 +1,13 @@
+package com.mycompany.ch02;
+
+public class ConstantsEx {
+
+	public static final double PAI = 3.14;
+	
+	public static void main(String[] args) {
+//		위에 PAI를 상수로 선언했기 때문에 다른 값을 대입할 수 없다.
+//		PAI = 11.1;
+		
+	}
+
+}
